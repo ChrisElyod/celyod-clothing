7456b71abcc3abfb1429e291256f84b5cde9aa41
ChrisElyod <sean.chris.doyle@gmail.com>
2021-09-15 10:23:29 -0400

    Added reselect library to project in order to update as little global state as possible

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
@@ -15,7 +16,7 @@ const CartIcon = ({ toggleCartHidden, totalItems }) => (
 )
 
 const mapStateToProps = state => ({
-  totalItems: state.cart.cartItems.length
+  totalItems: selectCartItemsCount(state)
 })
 
 const mapDispatchToProps = dispatch => ({
