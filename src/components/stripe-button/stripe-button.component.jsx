1f874d57a27eccf8aa01155a27ad4bf5079a870b
ChrisElyod <sean.chris.doyle@gmail.com>
2021-09-16 15:13:59 -0400

    Added stripe (test) payment integration and updated app title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51JaPdcASERS265ujtgfmrO5FU7GXRFyOJ4cErNMg4deH2qfjcxCAvFPEcR2Lwn0hmR2YGA1AXoQCl6tHQ12JMpKC00vcCehJQB';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CElyod Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+}
+
+export default StripeCheckoutButton;
